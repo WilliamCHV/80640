a566475d7ef108f7a736016934f87a9de6d3efc5
WilliamCHV <wil.chable2003@gmail.com>
2023-10-04 11:46:15 -0600

    practica06

---
+++ practica06.js
@@ -0,0 +1,7 @@
+const evento = document.getElementById("envio")
+formulario.addEventListener("submit",evento => {
+    evento.preventDefault()
+    let u = document.getElementById("user").value
+    let p = document.getElementById("password").value
+    alert(u+p)
+})
