9875284fef25bde7390653a04a9dd6ca92dd0cef
WilliamCHV <wil.chable2003@gmail.com>
2023-10-19 12:55:21 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,5 +1,7 @@
 package mx.uv;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,18 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/",
+            (request, response)->"<h1>Hola Mundo!</h1>"
+        );
+        get("/ruta1",
+            (request, response)->"<h1>Adios Mundo!</h1>"
+        );
+        get("/ruta2",
+            (request, response)->"<h1>Que Tal Mundo!</h1>"
+        );
+        get("/ruta3",
+            (request, response)->"<h1>OK Mundo!</h1>"
+        );
+
     }
 }
